de670546b223a3786c162dedc6f91578abb2240e
fmoissenet <florent.moissenet@protonmail.com>
2019-10-30 10:24:02 +0100

    Update algorithm from Ghousayni:

--- algorithm_Ghoussayni.m
+++ algorithm_Ghoussayni.m
@@ -5,7 +5,7 @@ cd('C:\Users\moissene\Documents\Professionnel\publications\articles\1- en cours\
 addpath(genpath('.\btk'));
 
 % Load 3D trajectories of markers and related frame rate and frame number
-c3Dfile = 'test.c3d';
+c3Dfile = './C3D Files/EVT127.c3d';
 btkData = btkReadAcquisition(c3Dfile);
 Rmarkers = btkGetMarkers(btkData);
 f = btkGetPointFrequency(btkData);
@@ -13,123 +13,135 @@ n = btkGetPointFrameNumber(btkData);
 
 % Filter 3D trajectories of markers
 % Butterworth filter, zero-phase filter, 2nd order, low-pass, cut-off 10 Hz
-rightFootMarkersName = {'R_FM5','R_FCC'};
+rightFootMarkersName = {'RDMT5','RHEE'};
 Fmarkers = [];
 for i = 1:length(rightFootMarkersName)
     [B,A] = butter(2,10/(f/2),'low');
     Fmarkers.(rightFootMarkersName{i}) = filtfilt(B, A, Rmarkers.(rightFootMarkersName{i}));
 end
 
-% Calculate the velocities of the markers in sagittal plane (along X axis)
+% Define gait axis and direction
+tdiff = Fmarkers.(rightFootMarkersName{i})(end,:) - Fmarkers.(rightFootMarkersName{i})(1,:);
+[~,gaitAxis] = max(abs(tdiff));
+verticalAxis = 3; % Z axis vertical
+if tdiff(gaitAxis) > 0
+    gaitDirection = 1;
+else
+    gaitDirection = -1;
+end
+
+% Calculate the velocities of the markers along gait (V1) and vertical (V2) axes
 for t = 1:n-1
     for i = 1:length(rightFootMarkersName)
-        Vmarkers.(rightFootMarkersName{i})(t) = (Fmarkers.(rightFootMarkersName{i})(t+1,1)- ...
-                                        Fmarkers.(rightFootMarkersName{i})(t,1))/ ...
-                                        (1/f);
+        V1markers.(rightFootMarkersName{i})(t) = gaitDirection*...
+                                                 (Fmarkers.(rightFootMarkersName{i})(t+1,gaitAxis)- ...
+                                                  Fmarkers.(rightFootMarkersName{i})(t,gaitAxis))/ ...
+                                                  (1/f);
+        V2markers.(rightFootMarkersName{i})(t) = abs(Fmarkers.(rightFootMarkersName{i})(t+1,verticalAxis)- ...
+                                                  Fmarkers.(rightFootMarkersName{i})(t,verticalAxis))/ ...
+                                                  (1/f);
     end
 end
-
-% Determine the average and standard deviation of sagittal velocities for
-% each marker during contact period by visual inspection (from 2 subjects,
-% and for each walking speed)
-startContact_S1_V1.(rightFootMarkersName{1}) = 24; % FCC from test.c3d
-stopContact_S1_V1.(rightFootMarkersName{1}) = 153; % FCC from test.c3d
-averageVelocity_S1_V1.(rightFootMarkersName{1}) = ...
-    mean(Vmarkers.R_FCC(startContact_S1_V1.(rightFootMarkersName{1}):...
-                        stopContact_S1_V1.(rightFootMarkersName{1})));
-stdVelocity_S1_V1.(rightFootMarkersName{1}) = ...
-    std(Vmarkers.R_FCC(startContact_S1_V1.(rightFootMarkersName{1}):...
-                        stopContact_S1_V1.(rightFootMarkersName{1})));
-% --
-startContact_S1_V1.(rightFootMarkersName{2}) = 11; % FM5 from test.c3d
-stopContact_S1_V1.(rightFootMarkersName{2}) = 200; % FM5 from test.c3d
-averageVelocity_S1_V1.(rightFootMarkersName{2}) = ...
-    mean(Vmarkers.R_FCC(startContact_S1_V1.(rightFootMarkersName{2}):...
-                        stopContact_S1_V1.(rightFootMarkersName{2})));
-stdVelocity_S1_V1.(rightFootMarkersName{2}) = ...
-    std(Vmarkers.R_FCC(startContact_S1_V1.(rightFootMarkersName{2}):...
-                        stopContact_S1_V1.(rightFootMarkersName{2})));
-                          
+                         
 % Velocity thresholds (empirically set)
 % 50 mm/s in the original article for barefoot gait
-potentialThresholds = [];
-for i = 1:length(rightFootMarkersName)
-    potentialThresholds = [potentialThresholds ...
-        averageVelocity_S1_V1.(rightFootMarkersName{i}) - ...
-        2*stdVelocity_S1_V1.(rightFootMarkersName{i})];
-end
-velocityThreshold = max(potentialThresholds);
+% 500 mm/s in Bruening et al., 2014
+% Adjusted for vertical velocity to 200 mm/s
+v1Threshold = 1000;
+v2Threshold = 200;
 
 % Detect events using the velocity threshold
-for i = 1:length(rightFootMarkersName)
-    Tevents(i).FS = [];
-    Tevents(i).FO = [];
-    for t = 1:n-1
-        if Vmarkers.(rightFootMarkersName{i})(t) >= velocityThreshold && ...
-           isempty(Tevents(i).FS)
-            Tevents(i).FS(1) = t;
+Tevents.R_FS = [];
+Tevents.R_FO = [];
+for t = 1:n-1
+    % Foot strike defined using heel marker
+    if isempty(Tevents.R_FS) && isempty(Tevents.R_FO)
+        if (V1markers.RHEE(t) <= v1Threshold && ...
+            V2markers.RHEE(t) <= v2Threshold)
+                Tevents.R_FS = [Tevents.R_FS t];
         end
-        if ~isempty(Tevents(i).FS)
-            if t > Tevents(i).FS(1)+n*0.1 && ... % move 10% of gait cycle from 1st FS
-               isempty(Tevents(i).FO) && ...
-               Vmarkers.(rightFootMarkersName{i})(t) < velocityThreshold
-                Tevents(i).FO(1) = t;
-            end
+    elseif ~isempty(Tevents.R_FS) && isempty(Tevents.R_FO)
+    	% Do nothing: wait for a first FO (assume that we detect first a FS)
+    elseif ~isempty(Tevents.R_FS) && ~isempty(Tevents.R_FO) && ...
+           length(Tevents.R_FS) > length(Tevents.R_FO)
+    	% Do nothing: wait for the next FO (assume that we detect first a FS)
+    elseif ~isempty(Tevents.R_FS) && ~isempty(Tevents.R_FO) && ...
+           length(Tevents.R_FS) == length(Tevents.R_FO)
+        if (V1markers.RHEE(t) <= v1Threshold && ...
+            V2markers.RHEE(t) <= v2Threshold)
+                Tevents.R_FS = [Tevents.R_FS t];
         end
-        if ~isempty(Tevents(i).FO) && length(Tevents(i).FS) <= 1
-            if t > Tevents(i).FO(1)+n*0.1 && ... % move 10% of gait cycle from 1st FO
-                Vmarkers.(rightFootMarkersName{i})(t) >= velocityThreshold
-                Tevents(i).FS(2) = t;
-            end
+    end
+    % Foot off defined using forefoot marker
+    if isempty(Tevents.R_FS) && isempty(Tevents.R_FO)
+        % Do nothing: wait for a first FS (assume that we detect first a FS)
+    elseif ~isempty(Tevents.R_FS) && isempty(Tevents.R_FO)
+        if (V1markers.RDMT5(t) >= v1Threshold && ...
+            V2markers.RDMT5(t) >= v2Threshold)
+                Tevents.R_FO = [Tevents.R_FO t];
+        end
+    elseif ~isempty(Tevents.R_FS) && ~isempty(Tevents.R_FO) && ...
+           length(Tevents.R_FO) < length(Tevents.R_FS)
+        if (V1markers.RDMT5(t) >= v1Threshold && ...
+            V2markers.RDMT5(t) >= v2Threshold)
+                Tevents.R_FO = [Tevents.R_FO t];
+        end
+    elseif ~isempty(Tevents.R_FS) && ~isempty(Tevents.R_FO) && ...
+           length(Tevents.R_FO) == length(Tevents.R_FS)
+        % Do nothing: wait for the nest FS (assume that we detect first a FS)
+    end
+end
+
+% Compare velocity threshold-based results with forceplate data (up to 4 PFs)
+temp = btkGetGroundReactionWrenches(btkData);
+for i = 1:length(temp)
+    for t = 1:length(temp(1).F(:,3))
+        if temp(i).F(t,3) < 10 % 10 N threshold on Z axis (vertical)
+            temp(i).F(t,:) = 0;
+        end
+    end
+    Forces(:,i) = interpft(temp(i).F(:,3),n); % Z axis (vertical)
+    temp1(i) = NaN;
+    if min(find(Forces(:,i)>1e-4))
+        temp1(i) = min(find(Forces(:,i)>1e-4));
+    end
+    if ~isnan(temp1(i))
+        temp2(i) = min(find(Forces(temp1(i):end,i)<1e-4)+temp1(i));
+    else
+        temp2(i) = NaN;
+    end
+    twindow = 10; % find similar event in a 10 frame window
+    for j = 1:length(Tevents.R_FO)
+        % Foot strike detection
+        if abs(temp1(i)-Tevents.R_FS(j)) <= twindow
+            mFS = temp1(i); % measured events
+            eFS = Tevents.R_FS(j); % estimated events
+        end
+        % Foot off detection
+        if abs(temp2(i)-Tevents.R_FO(j)) <= twindow
+            mFO = temp2(i); % measured events
+            eFO = Tevents.R_FO(j); % estimated events
         end
     end
 end
-tFS1 = [];
-tFS2 = [];
-tFO1 = [];
-for i = 1:length(rightFootMarkersName)
-    tFS1 = [tFS1 Tevents(i).FS(1)];    
-    tFS2 = [tFS2 Tevents(i).FS(2)];
-    tFO1 = [tFO1 Tevents(i).FO(1)];
-end
-Revents.FS(1) = min(tFS1);
-Revents.FS(2) = min(tFS2);
-Revents.FO(1) = max(tFO1);
 
 % Plot results
 figure;
 hold on;
-ylim([-3000 500]);
-for i = 1:length(rightFootMarkersName)
-    plot(Vmarkers.(rightFootMarkersName{i}));
-end
-line([Revents.FS(1) Revents.FS(1)],[-1e6 1e6],'Color','red','LineStyle','-');
-line([Revents.FS(2) Revents.FS(2)],[-1e6 1e6],'Color','red','LineStyle','-');
-line([Revents.FO(1) Revents.FO(1)],[-1e6 1e6],'Color','red','LineStyle','--');
+ylim([0 4000]);
+plot(V1markers.RHEE,'Color','blue');
+plot(V2markers.RHEE,'Color','green');
+plot(V1markers.RDMT5,'Color','blue','LineStyle','--');
+plot(V2markers.RDMT5,'Color','green','LineStyle','--');
+% for i = 1:length(Tevents.R_FO)
+%     line([Tevents.R_FS(i) Tevents.R_FS(i)],[-1e6 1e6],'Color','red','LineStyle','-');
+%     line([Tevents.R_FO(i) Tevents.R_FO(i)],[-1e6 1e6],'Color','red','LineStyle','--');
+% end
+line([mFS mFS],[-1e6 1e6],'Color','red','LineStyle','-');
+line([eFS eFS],[-1e6 1e6],'Color','blue','LineStyle','-');
+line([mFO mFO],[-1e6 1e6],'Color','red','LineStyle','--');
+line([eFO eFO],[-1e6 1e6],'Color','blue','LineStyle','--');
 
-% Compare velocity threshold-based results with forceplate data
-temp = btkGetGroundReactionWrenches(btkData);
-for t = 1:length(temp(1).F(:,3))
-    if temp(1).F(t,3) < 10 % 10 N threshold on Z axis (vertical)
-        temp(1).F(t,:) = 0;
-    end
-    if temp(2).F(t,3) < 10 % 10 N threshold on Z axis (vertical)
-        temp(2).F(t,:) = 0;
-    end
-end
-Forces = interpft([temp(1).F(:,3) temp(2).F(:,3)],n); % Z axis (vertical)
-temp11 = find(Forces(:,1)>1e-4);
-temp21 = find(Forces(:,2)>1e-4);
-temp12 = find(Forces(temp11(1):end,1)<1e-4)+temp11(1);
-temp22 = find(Forces(temp21(1):end,2)<1e-4)+temp21(1);
-if abs(temp11(1)-Revents.FS(1)) < abs(temp21(1)-Revents.FS(1))
-    eFS = temp11(1)-Revents.FS(1);
-    eFO = temp12(1)-Revents.FO(1);
-    line([temp11(1) temp11(1)],[-1e6 1e6],'Color','green','LineStyle','-');
-    line([temp12(1) temp12(1)],[-1e6 1e6],'Color','green','LineStyle','-');
-else
-    eFS = temp21(1)-Revents.FS(1);
-    eFO = temp22(1)-Revents.FO(1);
-    line([temp21(1) temp21(1)],[-1e6 1e6],'Color','green','LineStyle','-');
-    line([temp22(1) temp22(1)],[-1e6 1e6],'Color','green','LineStyle','-');
-end
+% Store errors (frames)
+errorFS = eFS - mFS
+errorFO = eFO - mFO
